9de6ddaff31c88458299b1edfcd044b3c4a40599
victorfdezc <victorfdezc1996@gmail.com>
2024-04-11 22:37:17 +0200

    Refact but now MNIST still works

--- tf-cvae.py
+++ tf-cvae.py
@@ -14,15 +14,20 @@ import tensorflow_docs.vis.embed as embed
 class CVAE(tf.keras.Model):
   """Convolutional variational autoencoder."""
 
-  def __init__(self, latent_dim):
+  def __init__(self, latent_dim, image_shape, img_channels=1):
     super(CVAE, self).__init__()
     self.latent_dim = latent_dim
     self.encoder = tf.keras.Sequential(
         [
-            tf.keras.layers.InputLayer(input_shape=(image_shape, image_shape, 3)),
-            tf.keras.layers.Conv2D(filters=16, kernel_size=3, strides=2, activation='relu'),
-            tf.keras.layers.Conv2D(filters=16, kernel_size=3, strides=2, activation='relu'),
+            tf.keras.layers.InputLayer(input_shape=(image_shape, image_shape, img_channels)),
+            tf.keras.layers.Conv2D(
+                filters=32, kernel_size=3, strides=(2, 2), activation='relu'),
+            tf.keras.layers.Conv2D(
+                filters=64, kernel_size=3, strides=(2, 2), activation='relu'),
+            tf.keras.layers.Conv2D(
+                filters=128, kernel_size=3, strides=(2, 2), activation='relu'),
             tf.keras.layers.Flatten(),
+            # No activation
             tf.keras.layers.Dense(latent_dim + latent_dim),
         ]
     )
@@ -30,12 +35,20 @@ class CVAE(tf.keras.Model):
     self.decoder = tf.keras.Sequential(
         [
             tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
-            tf.keras.layers.Dense(units=image_shape//2*image_shape//2*16, activation=tf.nn.relu),
-            tf.keras.layers.Reshape(target_shape=(image_shape//2, image_shape//2, 16)),
+            tf.keras.layers.Dense(units=7*7*32, activation=tf.nn.relu),
+            tf.keras.layers.Reshape(target_shape=(7, 7, 32)),
             tf.keras.layers.Conv2DTranspose(
-                filters=32, kernel_size=3, strides=2, padding='same', activation='relu'),
+                filters=128, kernel_size=3, strides=2, padding='same',
+                activation='relu'),
             tf.keras.layers.Conv2DTranspose(
-                filters=3, kernel_size=3, strides=1, padding='same', activation='sigmoid'),
+                filters=64, kernel_size=3, strides=2, padding='same',
+                activation='relu'),
+            tf.keras.layers.Conv2DTranspose(
+                filters=32, kernel_size=3, strides=1, padding='same',
+                activation='relu'),
+            # No activation
+            tf.keras.layers.Conv2DTranspose(
+                filters=img_channels, kernel_size=3, strides=1, padding='same'),
         ]
     )
 
@@ -73,7 +86,8 @@ def log_normal_pdf(sample, mean, logvar, raxis=1):
 def compute_loss(model, x):
   mean, logvar = model.encode(x)
   z = model.reparameterize(mean, logvar)
-  x_logit = tf.cast(model.decode(z), tf.float16)
+  ## TODO: FLOAT16? OR FLOAT32?
+  x_logit = tf.cast(model.decode(z), tf.float32)
   cross_ent = tf.cast(tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logit, labels=x), tf.float32)
   logpx_z = tf.cast(-tf.reduce_sum(cross_ent, axis=[1, 2, 3]), tf.float32)
   logpz = tf.cast(log_normal_pdf(z, 0., 0.), tf.float32)
@@ -101,16 +115,16 @@ def generate_and_save_images(model, epoch, test_sample):
   fig = plt.figure(figsize=(4, 4))
 
   for i in range(predictions.shape[0]):
-    plt.subplot(4, 4, i + 1)
+    plt.subplot(int(np.sqrt(predictions.shape[0])), int(np.sqrt(predictions.shape[0])), i + 1)
     plt.imshow(predictions[i, :, :, :])
     plt.axis('off')
 
   # tight_layout minimizes the overlap between 2 sub-plots
-  plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+  plt.savefig('data_out/image_at_epoch_{:04d}.png'.format(epoch))
   # plt.show()
 
-def preprocess_images(images):
-  images = images.reshape((images.shape[0], image_shape, image_shape, 3)).astype('float16') #/ 255.
+def preprocess_images(images, img_shape, channels):
+  images = (images.reshape((images.shape[0], img_shape, img_shape, channels)) / 255.).astype('float32')
   # return np.where(images > .5, 1.0, 0.0).astype('float32')
   return images
 
@@ -142,14 +156,14 @@ def plot_latent_images(model, n, digit_size=28):
   # plt.show()
 
 
-def extract_frames(video_path):
+def extract_frames(video_path, grayscale = False):
   video_reader = imageio.get_reader(video_path)
   frames = []
   for frame in video_reader:
     # Convert frame to PIL Image
     pil_frame = Image.fromarray(frame)
     # Convert to grayscale
-    # grayscale_frame = pil_frame.convert('L')
+    if grayscale: pil_frame = pil_frame.convert('L')
     # Resize frame to 28x28
     resized_frame = pil_frame.resize((image_shape, image_shape))
     # Divide each pixel value by 255 to normalize the image
@@ -157,36 +171,50 @@ def extract_frames(video_path):
     frames.append(normalized_frame)
   return frames
 
-def read_image(image_path):
+def read_image(image_path,channels=3):
   # Read an image file
-  image = imageio.imread(image_path)
-  # Get only RGB channels
-  image = image[:,:,:3]
+  image = PIL.Image.open(image_path)
+  if channels == 1: 
+    image = image.convert('L')
+    image = np.array(image)
+  elif channels == 3: 
+    # Get only RGB channels
+    image = np.array(image)
+    image = image[:,:,:channels]
   return np.array(image)
 
-def read_dataset(dataset_path):
+def read_dataset(dataset_path, channels=3):
   # Check if the path exists
-  if not os.path.exists(path):
+  if not os.path.exists(dataset_path):
       print("The specified path does not exist.")
       return
   # Check if the path is a directory
-  if not os.path.isdir(path):
+  if not os.path.isdir(dataset_path):
       print("The specified path is not a directory.")
       return
   # List all files in the directory
-  files = os.listdir(path)
+  files = os.listdir(dataset_path)
   
   dataset = []
   for file in files:
-    dataset.append(read_image(os.path.join(path, file)))
+    dataset.append(read_image(os.path.join(dataset_path, file), channels))
   return dataset
 
 def resize_image(image, image_shape):
+  # Extend dimension if grayscale
+  if len(np.shape(image)) == 2:  image = image[..., np.newaxis]
+  ## TODO: FLOAT16? OR FLOAT32?
   return tf.image.resize(image, [image_shape, image_shape])
 
-def normalize_image(image):
+def resize_dataset_images(dataset, image_shape):
+  resized_dataset = []
+  for image in dataset:
+    resized_dataset.append(resize_image(image, image_shape))
+  return np.array(resized_dataset)
+
+def normalize_images(images):
   # Normalize the image by 255
-  normalized_image = resized_image / 255.0
+  return np.array(images) / 255.0
 
 def inference_image(model, image):
   reshaped_image = tf.expand_dims(image, axis=0)
@@ -208,25 +236,24 @@ def split_frames(frames, num_images_set1):
     return set1, set2
 
 
-# train_size = 60000
-batch_size = 128
-# test_size = 10000
+batch_size = 256
 epochs = 100
 # set the dimensionality of the latent space to a plane for visualization later
-latent_dim = 32
+latent_dim = 128
 num_examples_to_generate = 16
-image_shape = 256
+img_shape = 28
+img_channels = 1
 
 
-# (train_images, _), (test_images, _) = tf.keras.datasets.mnist.load_data()
+(train_images, _), (test_images, _) = tf.keras.datasets.mnist.load_data()
 
 # frames = extract_frames("video_cortito.mp4")
 # print("Number of frames:", np.shape(frames))
 # train_images, test_images = split_frames(frames, 2400)
 
 # read images
-train_images = read_dataset("data_in/")
-train_images, test_images = split_frames(frames, 10)
+# train_images = read_dataset("data_in/selulitis/", img_channels)
+# train_images, test_images = split_frames(train_images, 1)
 
 # Get the data prepared
 train_size = len(train_images)
@@ -240,8 +267,20 @@ print("Shape of test_images:", np.shape(test_images))
 
 
 # Preprocess data
-train_images = preprocess_images(train_images)
-test_images = preprocess_images(test_images)
+train_images = resize_dataset_images(train_images, img_shape)
+train_images = preprocess_images(train_images,img_shape, img_channels)
+test_images = resize_dataset_images(test_images, img_shape)
+test_images = preprocess_images(test_images,img_shape, img_channels)
+
+# Get the data prepared
+train_size = len(train_images)
+test_size = len(test_images)
+train_images = np.array(train_images)
+test_images = np.array(test_images)
+print("Number of frames in train_images:", train_size)
+print("Number of frames in test_images:", test_size)
+print("Shape of train_images:", np.shape(train_images))
+print("Shape of test_images:", np.shape(test_images))
 
 # fig = plt.figure()
 # plt.imshow(test_images[0, :, :, :].astype('float32'))
@@ -273,11 +312,12 @@ optimizer = tf.keras.optimizers.Adam(1e-4)
 # it will be easier to see the improvement.
 random_vector_for_generation = tf.random.normal(
     shape=[num_examples_to_generate, latent_dim])
-model = CVAE(latent_dim)
+model = CVAE(latent_dim, img_shape, img_channels)
 
 # Pick a sample of the test set for generating output images
 assert batch_size >= num_examples_to_generate
 for test_batch in test_dataset.take(1):
+# for test_batch in train_dataset.take(1):
   test_sample = test_batch[0:num_examples_to_generate, :, :, :]
   
 generate_and_save_images(model, 0, test_sample)
@@ -300,9 +340,9 @@ for epoch in range(1, epochs + 1):
   #plt.imshow(display_image(epoch))
   #plt.axis('off')  # Display images
 
-anim_file = 'cvae.gif'
+anim_file = 'data_out/cvae.gif'
 with imageio.get_writer(anim_file, mode='I') as writer:
-  filenames = glob.glob('image*.png')
+  filenames = glob.glob('data_out/image*.png')
   filenames = sorted(filenames)
   for filename in filenames:
     image = imageio.imread(filename)
